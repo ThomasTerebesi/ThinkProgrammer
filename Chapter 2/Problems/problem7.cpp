569a33016444a4e32e78dd6408d92197eb71fbcc
Thomas Terebesi <thomasterebesi@googlemail.com>
2018-08-19 17:05:08 +0200

    'Decode a message', part 1 (41 - 48).

--- Chapter 2/Problems/problem7.cpp
+++ Chapter 2/Problems/problem7.cpp
@@ -1,10 +1,23 @@
 // "Problem: Decode a Message", page 41
 
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	std::cout << "Enter a number with as many digits as you like: ";
 
+	char digitChar = std::cin.get();
+	int number = (digitChar - '0');
+	digitChar = std::cin.get();
+
+	while (digitChar != 10)
+	{
+		number = number * 10 + (digitChar - '0');
+		digitChar = std::cin.get();
+	}
+
+	std::cout << "Number entered: " << number << "\n";
 
 	std::cin.get();
 
