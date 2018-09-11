34a67e46b94caf088f247a1479839af8720b39b6
Thomas <thomasterebesi@googlemail.com>
2018-09-11 18:59:10 +0200

    Linked lists (101 - 109).

--- Chapter 4/Problems/problem2.cpp
+++ Chapter 4/Problems/problem2.cpp
@@ -1,15 +1,49 @@
 // "Problem: Tracking an Unknown Quantity of Student Records", page 101
 
 #include <iostream>
+#include <memory>
 
 struct listNode
 {
 	int studentNum;
 	int grade;
 	listNode * next;
+
+	~listNode()
+	{
+		delete next;
+	}
 };
 
 typedef listNode * studentCollection;
+
+void AddRecord(studentCollection& sc, int stuNum, int stuGr)
+{
+	listNode * newNode = new listNode;
+	newNode->studentNum = stuNum;
+	newNode->grade = stuGr;
+	newNode->next = sc;
+	sc = newNode;
+}
+
+double AverageRecord(studentCollection sc)
+{
+	if (sc == nullptr)
+		return 0.0;
+
+	int count = 0;
+	double sum = 0;
+	listNode * loopPtr = sc;
+
+	while (loopPtr != nullptr)
+	{
+		sum += loopPtr->grade;
+		count++;
+		loopPtr = loopPtr->next;
+	}
+	
+	return sum / count;
+}
 
 int main()
 {
@@ -32,7 +66,11 @@ int main()
 	node2->next = node3;
 	node3->next = nullptr;
 
-	node1 = node2 = node3 = nullptr;
+	AddRecord(sc, 1274, 91);
+
+	double average = AverageRecord(sc);
+
+	std::cout << average << std::endl;
 	
 	std::cin.get();
 
