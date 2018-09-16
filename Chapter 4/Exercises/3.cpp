5acf255ef308ce4578a4e3cf3318fdf7d3470b7a
Thomas <thomasterebesi@googlemail.com>
2018-09-16 23:44:53 +0200

    Exercise 3, overengineered (109).

--- Chapter 4/Exercises/3.cpp
+++ Chapter 4/Exercises/3.cpp
@@ -71,33 +71,57 @@ arrayString Substring(const arrayString & s, int start, int length)
 
 void ReplaceString(arrayString& source, arrayString target, arrayString replaceText)
 {
+	int sourceLength = Length(source);
+	int targetLength = Length(target);
+	int replaceLength = Length(replaceText);
+	
+	arrayString checkString = new char[1];
+	checkString[0] = 0;
 	arrayString newString = new char[1];
 	newString[0] = 0;
 
-	for (int i = 0; i < Length(source); i++)
+	for (int i = 0; i < sourceLength; i++)
 	{
-		for (int j = 0; j < Length(target); j++)
+		// If the character does not equal the first target character append source character and break
+		if (source[i] != target[0])
 		{
-			if (source[i + j] != target[j])
-			{
-				Append(newString, source[i]);
-				break;
-			}
-
-			for (int k = 0; k < Length(replaceText); k++)
-			{
-				Append(newString, replaceText[k]);
-			}
-
-			i += Length(target);
+			Append(newString, source[i]);
+			continue;
 		}
+
+		// Store characters in checkString
+		for (int j = 0; j < targetLength; j++)
+		{
+			Append(checkString, source[i + j]);
+		}
+
+		// Check whether the string equals target
+		if (strcmp(checkString, target) == 0)
+		{
+			// Append each character of replaceText
+			for (int k = 0; k < replaceLength; k++)
+				Append(newString, replaceText[k]);
+		}
+		else
+		{
+			// Append each character of checkString
+			for (int k = 0; k < targetLength; k++)
+				Append(newString, checkString[k]);
+		}
+
+		delete[] checkString;
+		checkString = new char[1];
+		checkString[0] = 0;
+
+		i += targetLength - 1;
 	}
 
+	delete[] checkString;
 	delete[] source;
 	source = newString;
 }
 
-void ReplaceTester()
+void ReplaceTester1()
 {
 	arrayString source = new char[9];
 	source[0] = 'a';
@@ -122,7 +146,34 @@ void ReplaceTester()
 	replaceText[3] = 0;
 
 	std::cout << source << std::endl;
-	
+
+	ReplaceString(source, target, replaceText);
+
+	std::cout << source << std::endl;
+}
+
+void ReplaceTester2()
+{
+	arrayString source = new char[5];
+	source[0] = 'a';
+	source[1] = 'v';
+	source[2] = 'a';
+	source[3] = 'b';
+	source[4] = 0;
+
+	arrayString target = new char[3];
+	target[0] = 'a';
+	target[1] = 'b';
+	target[2] = 0;
+
+	arrayString replaceText = new char[4];
+	replaceText[0] = 'x';
+	replaceText[1] = 'y';
+	replaceText[2] = 'z';
+	replaceText[3] = 0;
+
+	std::cout << source << std::endl;
+
 	ReplaceString(source, target, replaceText);
 
 	std::cout << source << std::endl;
@@ -130,7 +181,11 @@ void ReplaceTester()
 
 int main()
 {
-	ReplaceTester();
+	ReplaceTester1();
+
+	std::cout << std::endl;
+
+	ReplaceTester2();
 
 	std::cin.get();
 
